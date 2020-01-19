3fc5dc56b051ea1f88734283645e02cf488dcae0
Attila Kocsis <kocsisa78@gmail.com>
2020-01-19 23:17:39 +0100

    creating bx_simd_t.h wip

--- example/tools/gen_simd_ amalgamated.cpp
+++ example/tools/gen_simd_ amalgamated.cpp
@@ -10,8 +10,90 @@
 
 #include <iostream>
 
+char* load(const char* _filename)
+{
+	bx::FileReader in;
+	bx::Error err;
+	in.open(bx::FilePath(_filename), &err);
+	
+	if (!err.isOk())
+	{
+		printf("cannot open input file: %s\n", _filename);
+		exit(-1);
+	}
+	
+	int64_t fileSize = bx::getSize(&in);
+	char* buffer = new char[fileSize + 1];
+	
+	bx::read(&in, buffer, (int32_t)fileSize);
+	buffer[fileSize] = 0;
+	
+	return buffer;
+}
+
+void process(bx::WriterI* _writer, char* _filename)
+{
+	char* buffer = load(_filename);
+	
+	int32_t len = bx::strLen(buffer);
+	char* temp = new char[len];
+	bx::StringView normalized = bx::normalizeEolLf(temp, len, buffer);
+	
+	for(bx::LineReader lr(normalized); !lr.isDone();)
+	{
+		bx::StringView line = lr.next();
+
+		bool writeLine = true;
+		
+		bx::StringView trimLine = bx::strLTrimSpace(line);
+		if ( trimLine.getLength() > 0 && trimLine.getPtr()[0] == '#')
+		{
+			bx::StringView token = bx::strLTrimSpace(bx::StringView(trimLine.getPtr()+1, trimLine.getTerm()));
+			
+			if ( 0 == bx::strCmp(token, "include", 7))
+			{
+				bx::StringView included = bx::strLTrimSpace(bx::StringView(token.getPtr()+7, token.getTerm()));
+				
+				included = bx::StringView(included.getPtr()+1, included.getTerm());
+				
+				bx::StringView term = bx::strFind(included, '"');
+				
+				if (!term.isEmpty())
+				{
+					included = bx::StringView(included.getPtr(), term.getPtr());
+					printf("%.*s\n", included.getLength(), included.getPtr());
+					writeLine = false;
+				}
+			}
+		}
+		
+		if ( writeLine )
+		{
+			bx::write(_writer, line);
+			if (!(bx::strLen(line) == 1 && line.getPtr()[0] == 10))
+			{
+				bx::write(_writer, "\n");
+			}
+		}
+	}
+	
+}
+
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	
+	bx::FileWriter out;
+	bx::Error err;
+	out.open("bx_simd_t.h", false, &err);
+	
+	if (!err.isOk())
+	{
+		printf("cannot open output file\n");
+		return -1;
+	}
+
+	process(&out, "simd_t.h");
+	
+	out.close();
+	
 	return 0;
 }
